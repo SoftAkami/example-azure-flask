000bd16d0ec73cf6164b85fc9bb9a39726dbb1d4
SoftAkami <joseph-cai@hotmail.com>
2022-02-11 23:22:39 -0500

    test

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins = "*")
 
+
 @app.route("/")
 def hello():
     return "Hello, SmartNinja! SocketIO1"
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
+    socketio.run(app, '127.0.0.1', port=8080)
